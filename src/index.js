bfaec4fef16575ed84402e2f3ea5d2ca677b5651
web3ten0 <web3ten0@gmail.com>
2023-02-06 13:07:00 +0900

    App Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
-root.render(<Welcome name="TAKAGI" />);
+const Welcome = (props) => <h1>Hello, {props.name}</h1>;
+const App = (props) => {
+  return (
+    <div>
+      <Welcome name="Taro" />
+      <Welcome name="Jiro" />
+      <Welcome name="Saburo" />
+    </div>
+  );
+};
+root.render(<App />);
