e3a9b3e317faca66834c2206b9f84ad30a4f8c6d
web3ten0 <web3ten0@gmail.com>
2023-02-06 14:35:40 +0900

    form, btn, preventDefault

--- src/index.js
+++ src/index.js
@@ -4,27 +4,15 @@ import "./index.css";
 // import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-  }
-  componentDidMount() {
-    this.timerId = setInterval(() => this.countUp(), 1000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerId);
-  }
-  countUp() {
-    this.setState((state) => ({ count: state.count + 1 }));
-  }
-  render() {
-    return <FormattedCount count={this.state.count} />;
-  }
-}
-function FormattedCount(props) {
-  return <h3>[COUNT]: {props.count}</h3>;
-}
-root.render(<Counter />);
+const Form = (props) => {
+  const submit = (e) => {
+    e.preventDefault();
+    console.log("submit!");
+  };
+  return (
+    <form onSubmit={submit}>
+      <button>hoge</button>
+    </form>
+  );
+};
+root.render(<Form />);
